1d9abc49a71148f112a7298db40e0642da3eecbd
PN-army <159958556+Paknodesarmy@users.noreply.github.com>
2024-07-11 00:57:03 -0700

    Update 0g_validator.sh

--- 0g/0g_validator.sh
+++ 0g/0g_validator.sh
@@ -11,73 +11,73 @@ echo "                                                                   |___/ "
 # Wait for 2 seconds
 sleep 2
 
-# Read node name from user input
+# set vars
 read -p "Enter node name: " MONIKER
-
-# Append environment variables to .bash_profile
-cat << EOF >> $HOME/.bash_profile
-export MONIKER="$MONIKER"
-export CHAIN_ID="zgtendermint_16600-2"
-export WALLET_NAME="wallet"
-export RPC_PORT="26657"
-EOF
-
-# Source .bash_profile to apply environment variables
+echo 'export MONIKER="'$MONIKER'"' >> $HOME/.bash_profile
+echo 'export CHAIN_ID="zgtendermint_16600-2"' >> ~/.bash_profile
+echo 'export WALLET_NAME="wallet"' >> ~/.bash_profile
+echo 'export RPC_PORT="26657"' >> ~/.bash_profile
 source $HOME/.bash_profile
-
-# Print node name
 echo '================================================='
 echo -e "Your node name: \e[1m\e[32m$MONIKER\e[0m"
 echo '================================================='
+sleep 2
 
-# Update and upgrade system packages
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
 sudo apt update && sudo apt upgrade -y
 
-# Install necessary dependencies
 echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
-sudo apt install -y curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential bsdmainutils git make ncdu gcc chrony liblz4-tool snapd
-
-# Install Go if not already installed
-echo -e "\e[1m\e[32mInstalling Go... \e[0m" && sleep 1
-GO_VERSION="1.22.0"
-wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
-sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
-rm "go$GO_VERSION.linux-amd64.tar.gz"
-echo "export PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+# packages
+sudo apt install curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential protobuf-compiler bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y snapd
+# install go
+cd $HOME && \
+ver="1.22.0" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+source $HOME/.bash_profile && \
 go version
 sleep 1
 
-# Download and build 0g-chain binaries
-echo -e "\e[1m\e[32m3. Downloading and building 0g-chain binaries... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+
+# download binary
+
 git clone -b v0.2.3 https://github.com/0glabs/0g-chain.git
 cd 0g-chain
 make install
 0gchaind version
 
-# Initialize 0gchain node
-echo -e "\e[1m\e[32mInitializing 0gchain node... \e[0m" && sleep 1
+# init node
+
+cd $HOME
 0gchaind init $MONIKER --chain-id $CHAIN_ID
 0gchaind config chain-id $CHAIN_ID
 0gchaind config node tcp://localhost:$RPC_PORT
-0gchaind config keyring-backend os
+0gchaind config keyring-backend os # You can set it to "test" so you will not be asked for a password
 
-# Download genesis file
+# download genesis
+
 rm $HOME/.0gchain/config/genesis.json
 wget https://github.com/0glabs/0g-chain/releases/download/v0.2.3/genesis.json -O $HOME/.0gchain/config/genesis.json
 
-# Configure seeds and persistent peers
-SEEDS="81987895a11f6689ada254c6b57932ab7ed909b6@54.241.167.190:26656,010fb4de28667725a4fef26cdc7f9452cc34b16d@54.176.175.48:26656,e9b4bc203197b62cc7e6a80a64742e752f4210d5@54.193.250.204:26656,68b9145889e7576b652ca68d985826abd46ad660@18.166.164.232:26656"
-PEERS="6dbb0450703d156d75db57dd3e51dc260a699221@152.53.47.155:13456,1bf93ac820773970cf4f46a479ab8b8206de5f60@62.171.185.81:12656,df4cc52fa0fcdd5db541a28e4b5a9c6ce1076ade@37.60.246.110:13456,66d59739b6b4ff0658e63832a5bbeb29e5259742@144.76.79.209:26656,76cc5b9beaff9f33dc2a235e80fe2d47448463a7@95.216.114.170:26656,adc616f440155f4e5c2bf748e9ac3c9e24bf78ac@51.161.13.62:26656,cd662c11f7b4879b3861a419a06041c782f1a32d@89.116.24.249:26656,40cf5c7c11931a4fdab2b721155cc236dfe7a809@84.46.255.133:12656,11945ced69c3448adeeba49355703984fcbc3a1a@37.27.130.146:26656,c02bf872d61f5dd04e877105ded1bd03243516fb@65.109.25.252:12656"
-sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" $HOME/.0gchain/config/config.toml
-sed -i "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.0gchain/config/config.toml
+# add seed and peer
 
-# Set minimum gas price
+
+SEEDS="81987895a11f6689ada254c6b57932ab7ed909b6@54.241.167.190:26656,010fb4de28667725a4fef26cdc7f9452cc34b16d@54.176.175.48:26656,e9b4bc203197b62cc7e6a80a64742e752f4210d5@54.193.250.204:26656,68b9145889e7576b652ca68d985826abd46ad660@18.166.164.232:26656" && \
+sed -i.bak -e "s/^seeds *=.*/seeds = \"${SEEDS}\"/" $HOME/.0gchain/config/config.toml
+PEERS="6dbb0450703d156d75db57dd3e51dc260a699221@152.53.47.155:13456,1bf93ac820773970cf4f46a479ab8b8206de5f60@62.171.185.81:12656,df4cc52fa0fcdd5db541a28e4b5a9c6ce1076ade@37.60.246.110:13456,66d59739b6b4ff0658e63832a5bbeb29e5259742@144.76.79.209:26656,76cc5b9beaff9f33dc2a235e80fe2d47448463a7@95.216.114.170:26656,adc616f440155f4e5c2bf748e9ac3c9e24bf78ac@51.161.13.62:26656,cd662c11f7b4879b3861a419a06041c782f1a32d@89.116.24.249:26656,40cf5c7c11931a4fdab2b721155cc236dfe7a809@84.46.255.133:12656,11945ced69c3448adeeba49355703984fcbc3a1a@37.27.130.146:26656,c02bf872d61f5dd04e877105ded1bd03243516fb@65.109.25.252:12656,d5e294d6d5439f5bd63d1422423d7798492e70fd@77.237.232.146:26656,386c82b09e0ec6a68e653a5d6c57f766ae73e0df@194.163.183.208:26656,4eac33906b2ba13ab37d0e2fe8fc5801e75f25a0@154.38.168.168:13456,c96b65a5b02081e3111b8b38cd7f5df76c7f9404@185.182.185.160:26656,48e3cab55ba7a1bc8ea940586e4718a857de84c4@178.63.4.186:26656"
+sed -i "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.0gchain/config/config.toml
+# set min gas price
+
 sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ua0gi\"/" $HOME/.0gchain/config/app.toml
 
-# Create systemd service for 0gchain node
-sudo tee /etc/systemd/system/0gd.service > /dev/null << EOF
+# create service
+
+sudo tee /etc/systemd/system/0gd.service > /dev/null <<EOF
 [Unit]
 Description=0G Node
 After=network.target
@@ -93,8 +93,12 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-# Start 0gchain node service
-sudo systemctl daemon-reload && sudo systemctl enable 0gd && sudo systemctl restart 0gd && sudo journalctl -u 0gd -f -o cat
+# start service
+
+sudo systemctl daemon-reload && \
+sudo systemctl enable 0gd && \
+sudo systemctl restart 0gd && \
+sudo journalctl -u 0gd -f -o cat
 
 # Print setup finished message
 echo '=============== SETUP FINISHED ==================='
