8dc37356fa2ed420db21a8daa6a2c421101c0b3c
PN-army <159958556+Paknodesarmy@users.noreply.github.com>
2024-07-13 02:45:50 -0700

    Update og-storage.sh

--- 0g/og-storage.sh
+++ 0g/og-storage.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 0G Storage Node Auto-Installation Script
-
 echo "   ___      _        __          _              _                        "
 echo "  / _ \__ _| | __ /\ \ \___   __| | ___  ___   /_\  _ __ _ __ ___  _   _ "
 echo " / /_)/ _  | |/ //  \/ / _ \ / _  |/ _ \/ __| //_\\| '__| '_  _ \| | | |"
@@ -13,9 +11,9 @@ echo "                                                                   |___/ "
 sleep 2
 
 # Install dependencies for building from source
-echo -e "\e[1m\e[32m1. Installing dependencies... \e[0m" && sleep 1
-sudo apt-get update
-sudo apt-get install clang cmake build-essential -y
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y clang cmake build-essential jq
 
 # Install Go
 echo -e "\e[1m\e[32m2. Installing Go... \e[0m" && sleep 1
@@ -29,14 +27,16 @@ echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
 source ~/.bash_profile && \
 go version
 
-# Install rustup
+# Install Rust
 echo -e "\e[1m\e[32m3. Installing Rust... \e[0m" && sleep 1
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+. "$HOME/.cargo/env"
 
 # Set environment variables
 echo -e "\e[1m\e[32m4. Setting environment variables... \e[0m" && sleep 1
-read -p "Enter json-rpc: " BLOCKCHAIN_RPC_ENDPOINT && echo "Current json-rpc: $BLOCKCHAIN_RPC_ENDPOINT"
+read -p "Enter json-rpc: " BLOCKCHAIN_RPC_ENDPOINT
+echo "Current json-rpc: $BLOCKCHAIN_RPC_ENDPOINT"
+
 ENR_ADDRESS=$(wget -qO- eth0.me)
 echo "export ENR_ADDRESS=${ENR_ADDRESS}" >> ~/.bash_profile
 echo 'export ZGS_LOG_DIR="$HOME/0g-storage-node/run/log"' >> ~/.bash_profile
@@ -44,15 +44,14 @@ echo 'export ZGS_LOG_SYNC_BLOCK="802"' >> ~/.bash_profile
 echo 'export LOG_CONTRACT_ADDRESS="0x8873cc79c5b3b5666535C825205C9a128B1D75F1"' >> ~/.bash_profile
 echo 'export MINE_CONTRACT="0x85F6722319538A805ED5733c5F4882d96F1C7384"' >> ~/.bash_profile
 echo "export BLOCKCHAIN_RPC_ENDPOINT=\"$BLOCKCHAIN_RPC_ENDPOINT\"" >> ~/.bash_profile
+
 source ~/.bash_profile
 
-echo -e "\n\033[31mCHECK YOUR STORAGE NODE VARIABLES\033[0m\n\nLOG_CONTRACT_ADDRESS: $LOG_CONTRACT_ADDRESS\nMINE_CONTRACT: $MINE_CONTRACT\nZGS_LOG_SYNC_BLOCK: $ZGS_LOG_SYNC_BLOCK\nBLOCKCHAIN_RPC_ENDPOINT: $BLOCKCHAIN_RPC_ENDPOINT\n\n" "\033[3m\"lets buidl together\" - Grand Valley\033[0m"
-
-# Verify JSON-RPC sync
+echo -e "\n\033[31mCHECK YOUR STORAGE NODE VARIABLES\033[0m\n\nLOG_CONTRACT_ADDRESS: $LOG_CONTRACT_ADDRESS\nMINE_CONTRACT: $MINE_CONTRACT\nZGS_LOG_SYNC_BLOCK: $ZGS_LOG_SYNC_BLOCK\nBLOCKCHAIN_RPC_ENDPOINT: $BLOCKCHAIN_RPC_ENDPOINT\n\n\033[3m\"lets buidl together\" - Grand Valley\033[0m"
 curl -s -X POST $BLOCKCHAIN_RPC_ENDPOINT -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r '.result' | xargs printf "%d\n"
 
-# Download and build the binary
-echo -e "\e[1m\e[32m5. Downloading and building the binary... \e[0m" && sleep 1
+# Download binary
+echo -e "\e[1m\e[32m5. Downloading and building binaries... \e[0m" && sleep 1
 cd $HOME
 git clone https://github.com/0glabs/0g-storage-node.git
 cd $HOME/0g-storage-node
@@ -61,38 +60,38 @@ git tag -d v0.3.3
 git fetch --all --tags
 git checkout 2a2688d2c34a1e9480239e17b626912370662dcc
 git submodule update --init
-sudo apt install cargo -y
+sudo apt install -y cargo
 cargo build --release
 
-# Wallet private key check
-echo -e "\e[1m\e[32m6. Wallet private key check... \e[0m" && sleep 1
+# Obtain wallet private key
+echo -e "\e[1m\e[32m6. Obtaining wallet private key... \e[0m" && sleep 1
 read -sp "Enter your private key: " PRIVATE_KEY && echo
 
 # Update node configuration
 echo -e "\e[1m\e[32m7. Updating node configuration... \e[0m" && sleep 1
-sed -i '
-s|^miner_key = ""|miner_key = "'"$PRIVATE_KEY"'"|
-s|^\s*#\?\s*network_dir\s*=.*|network_dir = "network"|
-s|^\s*#\?\s*network_enr_address\s*=.*|network_enr_address = "'"$ENR_ADDRESS"'"|
+sed -i "
+s|^miner_key = \"\"|miner_key = \"$PRIVATE_KEY\"|
+s|^\s*#\?\s*network_dir\s*=.*|network_dir = \"network\"|
+s|^\s*#\?\s*network_enr_address\s*=.*|network_enr_address = \"$ENR_ADDRESS\"|
 s|^\s*#\?\s*network_enr_tcp_port\s*=.*|network_enr_tcp_port = 1234|
 s|^\s*#\?\s*network_enr_udp_port\s*=.*|network_enr_udp_port = 1234|
 s|^\s*#\?\s*network_libp2p_port\s*=.*|network_libp2p_port = 1234|
 s|^\s*#\?\s*network_discovery_port\s*=.*|network_discovery_port = 1234|
-s|^\s*#\s*rpc_listen_address\s*=.*|rpc_listen_address = "0.0.0.0:5678"|
-s|^\s*#\s*rpc_listen_address_admin\s*=.*|rpc_listen_address_admin = "0.0.0.0:5679"|
+s|^\s*#\s*rpc_listen_address\s*=.*|rpc_listen_address = \"0.0.0.0:5678\"|
+s|^\s*#\s*rpc_listen_address_admin\s*=.*|rpc_listen_address_admin = \"0.0.0.0:5679\"|
 s|^\s*#\?\s*rpc_enabled\s*=.*|rpc_enabled = true|
-s|^\s*#\?\s*db_dir\s*=.*|db_dir = "db"|
-s|^\s*#\?\s*log_config_file\s*=.*|log_config_file = "log_config"|
-s|^\s*#\?\s*log_directory\s*=.*|log_directory = "log"|
-s|^\s*#\?\s*network_boot_nodes\s*=.*|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS","/ip4/18.167.69.68/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"]|
-s|^\s*#\?\s*log_contract_address\s*=.*|log_contract_address = "'"$LOG_CONTRACT_ADDRESS"'"|
-s|^\s*#\?\s*mine_contract_address\s*=.*|mine_contract_address = "'"$MINE_CONTRACT"'"|
-s|^\s*#\?\s*log_sync_start_block_number\s*=.*|log_sync_start_block_number = '"$ZGS_LOG_SYNC_BLOCK"'|
-s|^\s*#\?\s*blockchain_rpc_endpoint\s*=.*|blockchain_rpc_endpoint = "'"$BLOCKCHAIN_RPC_ENDPOINT"'"|
-' $HOME/0g-storage-node/run/config.toml
+s|^\s*#\?\s*db_dir\s*=.*|db_dir = \"db\"|
+s|^\s*#\?\s*log_config_file\s*=.*|log_config_file = \"log_config\"|
+s|^\s*#\?\s*log_directory\s*=.*|log_directory = \"log\"|
+s|^\s*#\?\s*network_boot_nodes\s*=.*|network_boot_nodes = \[\"/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps\",\"/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS\",\"/ip4/18.167.69.68/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX\"]|
+s|^\s*#\?\s*log_contract_address\s*=.*|log_contract_address = \"$LOG_CONTRACT_ADDRESS\"|
+s|^\s*#\?\s*mine_contract_address\s*=.*|mine_contract_address = \"$MINE_CONTRACT\"|
+s|^\s*#\?\s*log_sync_start_block_number\s*=.*|log_sync_start_block_number = $ZGS_LOG_SYNC_BLOCK|
+s|^\s*#\?\s*blockchain_rpc_endpoint\s*=.*|blockchain_rpc_endpoint = \"$BLOCKCHAIN_RPC_ENDPOINT\"|
+" $HOME/0g-storage-node/run/config.toml
 
 # Create service
-echo -e "\e[1m\e[32m8. Creating service... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m8. Creating systemd service... \e[0m" && sleep 1
 sudo tee /etc/systemd/system/zgs.service > /dev/null <<EOF
 [Unit]
 Description=ZGS Node
@@ -112,9 +111,9 @@ EOF
 
 # Start the node
 echo -e "\e[1m\e[32m9. Starting the node... \e[0m" && sleep 1
-sudo systemctl daemon-reload && \
-sudo systemctl enable zgs && \
-sudo systemctl start zgs && \
+sudo systemctl daemon-reload
+sudo systemctl enable zgs
+sudo systemctl start zgs
 sudo systemctl status zgs
 
 # Show logs by date
